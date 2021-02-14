dcaf5d8238ff81a62fe32847e454966c44f0d777
shiroino111 <meijifms3rdclass3@gmail.com>
2021-02-14 21:34:33 +0900

    [clean]pep8順守

--- titanic_083.py
+++ titanic_083.py
@@ -10,10 +10,9 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn import model_selection
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-from sklearn.feature_selection import SelectKBest
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.pipeline import Pipeline
 
 warnings.filterwarnings('ignore')
 
@@ -39,10 +38,6 @@ dataset["Age"].fillna(dataset.Age.mean(), inplace=True)
 dataset["Fare"].fillna(dataset.Fare.mean(), inplace=True) 
 dataset["Embarked"].fillna("S", inplace=True)
 
-# 全体の欠損データの個数を確認
-dataset_null = dataset.fillna(np.nan)
-dataset_null.isnull().sum()
-
 # 使用する変数を抽出
 dataset2 = dataset[['Survived', 'Pclass', 'Sex', 'Age', 'Fare', 'Embarked', 'Parch', 'SibSp']]
 
@@ -56,17 +51,16 @@ del test_set["Survived"]
 
 # trainデータを変数と正解に分離
 X = train_set.as_matrix()[:, 1:] # Pclass以降の変数
-y = train_set.as_matrix()[:, 0] # 正解データ
+y = train_set.as_matrix()[:, 0]  # 正解データ
 
 # 予測モデルの作成
 clf = RandomForestClassifier(random_state=10, max_features='sqrt')
 pipe = Pipeline([('classify', clf)])
-param_test = {'classify__n_estimators': list(range(20, 30, 1)), # 20～30を１刻みずつ試す
-              'classify__max_depth': list(range(3, 10, 1))}     # 3～10を１刻みずつ試す
-grid = GridSearchCV(estimator = pipe, param_grid = param_test, scoring='accuracy', cv=10)
+param_test = {'classify__n_estimators': list(range(20, 30, 1)),  # 20～30を１刻みずつ試す
+              'classify__max_depth': list(range(3, 10, 1))}      # 3～10を１刻みずつ試す
+grid = GridSearchCV(estimator = pipe, param_grid=param_test, scoring='accuracy', cv=10)
 grid.fit(X, y)
 print(grid.best_params_, grid.best_score_, sep="\n")
-
 
 # testデータの予測
 pred = grid.predict(test_set)
